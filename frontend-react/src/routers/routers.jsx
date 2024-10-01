6fa26e31339ab8364d2de62c5ec2b7d7a0d84610
Yitian Yang <yyan5712@uni.sydney.edu.au>
2024-10-01 14:12:09 +1000

    feat: add landing page

--- frontend-react/src/routers/routers.jsx
+++ frontend-react/src/routers/routers.jsx
@@ -5,11 +5,12 @@ import WelcomeLayout from "../layouts/WelcomeLayout.jsx";
 
 import SciptManagementPage from "../pages/ScriptManagementPage.jsx";
 import ContextManagementPage from "../pages/ContextManagementPage.jsx";
+import LandingPage from "../pages/LandingPage.jsx";
 
 function ProjectRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<CommonLayout><SciptManagementPage /></CommonLayout>}></Route>
+            <Route path="/" element={<WelcomeLayout><LandingPage /></WelcomeLayout>}></Route>
             <Route path="/context" element={<CommonLayout><ContextManagementPage /></CommonLayout>}></Route>
             {/*<Route path="/Login" element={<CommonLayout><SignUpPage /></CommonLayout>}></Route>*/}
             {/*<Route path="/" element={<CommonLayout><ContextManagementPage /></CommonLayout>}></Route>*/}
