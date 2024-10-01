7bb1d7cd71c4b8ccaee674017925d5dfb883f1b9
Yitian Yang <yyan5712@uni.sydney.edu.au>
2024-10-01 17:54:44 +1000

    feat: add context, script creation table, add auto_increment to context_references and scripts_relation table

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -1,10 +1,11 @@
+Drop table if exists users;
+Drop table if exists scripts;
 Drop table if exists scripts_relation;
-Drop table if exists context_reference;
 Drop table if exists script_scenes;
 Drop table if exists context;
-Drop table if exists scripts;
-Drop table if exists users;
+Drop table if exists context_reference;
 
+-- create table users
 Create table users
 (
     id            BIGINT,
@@ -33,11 +34,12 @@ DROP TABLE IF EXISTS scripts;
 -- 创建脚本表
 CREATE TABLE scripts
 (
-    id          BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY, -- 主键,自增
+    id          BIGINT  NOT NULL AUTO_INCREMENT, -- auto increment
     creator     BIGINT  NOT NULL, -- 外键，指向用户表
     name CHAR(50) NOT NULL,       -- 脚本名称
     create_time DATETIME,         -- 创建时间
     update_time DATETIME,         -- 更新时间
+    PRIMARY KEY (id), -- primary key
     FOREIGN KEY (creator) REFERENCES users(id)
 );
 
@@ -80,19 +82,55 @@ CREATE TRIGGER before_update_script_scenes
     ON script_scenes
     FOR EACH ROW SET NEW.update_time = NOW();
 
+CREATE TABLE scripts_creation
+(
+    id          BIGINT   NOT NULL AUTO_INCREMENT,   -- Primary key, auto-increment
+    positive    TEXT     NOT NULL,                   -- Positive text, cannot be null
+    negative    TEXT     NOT NULL,                   -- Negative text, cannot be null
+    script_id   BIGINT      NOT NULL,                   -- Foreign key, cannot be null
+    PRIMARY KEY (id),                                -- Primary key
+    FOREIGN KEY (script_id) REFERENCES scripts (id) -- Link this table to script table
+);
+
+-- create table context
+CREATE TABLE context
+(
+    id          BIGINT   NOT NULL AUTO_INCREMENT,   -- primary key, auto_increment
+    title       CHAR(50) NOT NULL,                  -- title
+    description    CHAR(50) NOT NULL,               -- description
+    positive    TEXT     NOT NULL,                   -- positive context
+    negative    TEXT     NOT NULL,                   -- negative context
+    creator     BIGINT   NOT NULL,                  -- creator, which is a foreign key
+    create_time DATETIME,                           -- create_time
+    update_time DATETIME,                           -- update_time
+    PRIMARY KEY (id),
+    FOREIGN KEY (creator) REFERENCES users (id)     -- link this table to users table
+);
+
+-- set trigger
+CREATE TRIGGER before_insert_context
+    BEFORE INSERT
+    ON context
+    FOR EACH ROW SET NEW.create_time = NOW();
+
+CREATE TRIGGER before_update_context
+    BEFORE UPDATE
+    ON context
+    FOR EACH ROW SET NEW.update_time = NOW();
+
 -- create table context reference
 Create table context_reference(
-    id BIGINT,
+    id  BIGINT  NOT NULL AUTO_INCREMENT,
     script_id BIGINT NOT NULL ,
     context_id BIGINT NOT NULL ,
     PRIMARY KEY (id),
-    FOREIGN KEY (script_id) REFERENCES scripts(id)
-#     FOREIGN KEY (context_id) REFERENCES context(id)
+    FOREIGN KEY (script_id) REFERENCES scripts(id),
+    FOREIGN KEY (context_id) REFERENCES context(id)
 );
 
 -- create table scripts relation
 Create table scripts_relation(
-    id BIGINT,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     parent_id BIGINT NOT NULL,
     child_id BIGINT NOT NULL,
     relation char(50) NOT NULL,
