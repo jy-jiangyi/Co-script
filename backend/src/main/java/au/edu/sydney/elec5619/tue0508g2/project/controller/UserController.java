e621b3c801884c395ec9c5669f4a2678827d07ee
Yitian Yang <yyan5712@uni.sydney.edu.au>
2024-10-11 14:09:53 +1100

    feat: add new backend api /users/register

--- backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/controller/UserController.java
+++ backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/controller/UserController.java
@@ -3,10 +3,12 @@ package au.edu.sydney.elec5619.tue0508g2.project.controller;
 import au.edu.sydney.elec5619.tue0508g2.project.entity.Users;
 import au.edu.sydney.elec5619.tue0508g2.project.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping(path="/users")
@@ -19,6 +21,29 @@ public class UserController {
     public @ResponseBody Iterable<Users> getAllUsers() {
         // This returns a JSON or XML with the users
         return usersRepository.findAll();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Users user) {
+
+        // hash password but do not write unill now
+        user.setPassword_hash(user.getPassword_hash());
+
+        // create create_time and update_time
+        LocalDateTime now = LocalDateTime.now();
+        user.setCreate_time(now);
+        user.setUpdate_time(now);
+
+        try {
+            // save user data
+            Users savedUser = usersRepository.save(user);
+
+            // return 201 status code and created succesfully msg
+            return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully.");
+        } catch (Exception e) {
+            // return bad request status code and possible error msg
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Registration failed: " + e.getMessage());
+        }
     }
 
 }
