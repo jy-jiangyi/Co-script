d0b5521312eb8accf166b882e492f33ce2814860
Hongyi Sun <hsun0778@uni.sydney.edu.au>
2024-10-13 16:43:07 +1100

    fix: change ScriptRepository Type to Long

--- backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/repository/ScriptRepository.java
+++ backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/repository/ScriptRepository.java
@@ -2,6 +2,6 @@ package au.edu.sydney.elec5619.tue0508g2.project.repository;
 import au.edu.sydney.elec5619.tue0508g2.project.entity.Script;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ScriptRepository extends CrudRepository<Script, Integer> {
+public interface ScriptRepository extends CrudRepository<Script, Long> {
 
 }
