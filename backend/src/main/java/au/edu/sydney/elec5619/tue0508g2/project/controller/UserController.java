cd54ec4a3b33dad93850beb669802eec814ac523
Yitian Yang <yyan5712@uni.sydney.edu.au>
2024-10-15 16:24:57 +1100

    feat: add user login function

--- backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/controller/UserController.java
+++ backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/controller/UserController.java
@@ -2,15 +2,18 @@ package au.edu.sydney.elec5619.tue0508g2.project.controller;
 
 import au.edu.sydney.elec5619.tue0508g2.project.entity.Users;
 import au.edu.sydney.elec5619.tue0508g2.project.repository.UsersRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.net.ssl.HandshakeCompletedEvent;
 import java.time.LocalDateTime;
 
-@Controller
+@RestController
 @RequestMapping(path="/users")
 public class UserController {
 
@@ -30,7 +33,7 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("Email already in use, please choose another one.");
         }
-        // hash password but do not write unill now
+        // add hash password function
         user.setPassword_hash(user.getPassword_hash());
 
         // create create_time and update_time
@@ -48,6 +51,29 @@ public class UserController {
             // return bad request status code and possible error msg
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Registration failed: " + e.getMessage());
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody Users user, HttpServletRequest request) {
+        // 检查用户是否存在
+        Users existingUser = usersRepository.findByEmail(user.getEmail());
+        if (existingUser == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid email or password.");
+        }
+
+        // compared password directly, configure security configuration in future
+        if (!existingUser.getPassword_hash().equals(user.getPassword_hash())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid email or password.");
+        }
+
+        // set session
+        HttpSession session = request.getSession();
+        session.setAttribute("userId", existingUser.getId());
+
+        // 登录成功，返回 200 状态码和成功消息
+        return ResponseEntity.status(HttpStatus.OK).body("Login successful.");
     }
 
 }
