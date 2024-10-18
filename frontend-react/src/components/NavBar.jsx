b17c308e564be5273ab590578dc5664f83e2ec93
Yitian Yang <yyan5712@uni.sydney.edu.au>
2024-10-18 14:50:50 +1100

    feat: edit navbar

--- frontend-react/src/components/NavBar.jsx
+++ frontend-react/src/components/NavBar.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import {Menu} from 'antd';
-
-const items1 = ['1', '2', '3'].map((key) => ({
-    key,
-    label: `nav ${key}`,
-}));
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            items={items1}
-            style={{
-                flex: 1,
-                minWidth: 0,
-            }}
-        />
-    )
-        ;
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+                <Link to="/home">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+                <Link to="/scene_illustration">Scene Illustration</Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+                <Link to="/script_editing">Script Editing</Link>
+            </Menu.Item>
+            <Menu.Item key="4">
+                <Link to="/context">Context Management</Link>
+            </Menu.Item>
+        </Menu>
+    );
 };
 
 export default NavBar;
