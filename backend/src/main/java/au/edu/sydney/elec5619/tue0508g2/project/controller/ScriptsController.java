908baf181cd057dc1f58cd5c26670224b2a067d8
Yitian Yang <yyan5712@uni.sydney.edu.au>
2024-10-18 14:49:41 +1100

    feat: add new get scriptcontent by id method

--- backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/controller/ScriptsController.java
+++ backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/controller/ScriptsController.java
@@ -4,8 +4,12 @@ import au.edu.sydney.elec5619.tue0508g2.project.entity.ScriptScenes;
 import au.edu.sydney.elec5619.tue0508g2.project.repository.ScriptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import au.edu.sydney.elec5619.tue0508g2.project.entity.Script;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/scripts")
@@ -14,10 +18,23 @@ public class ScriptsController {
     @Autowired
     private ScriptRepository scriptRepository;
 
+    // get specific scene
+
     // 调AI生成Script
 
     // save AI生成的Script
 
     // update 我的Script
+
+    //return script content
+    @GetMapping("/{id}")
+    public ResponseEntity<Script> getScriptById(@PathVariable Long id) {
+        Optional<Script> scriptOptional = scriptRepository.findById(id);
+        if (scriptOptional.isPresent()) {
+            return ResponseEntity.ok(scriptOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
