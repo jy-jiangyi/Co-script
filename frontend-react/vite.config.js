6e4f362b5ea1dba07ce1b45255607ae0f5714052
Yitian Yang <yyan5712@uni.sydney.edu.au>
2024-10-19 22:43:38 +1100

    feat: manage router

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -17,6 +17,11 @@ export default defineConfig({
         target: "http://localhost:8080/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/user/, "user"),
+      },
+      "/scripts": {
+        target: "http://localhost:8080/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/user/, "user"),
       }
     }
   }
