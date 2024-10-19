8d30a63bab7d37ee0bb1d3e6ff196a62f2d9e616
Yitian Yang <yyan5712@uni.sydney.edu.au>
2024-10-19 22:50:21 +1100

    feat: adjust navbar

--- frontend-react/src/components/NavBar.jsx
+++ frontend-react/src/components/NavBar.jsx
@@ -9,12 +9,6 @@ const NavBar = () => {
                 <Link to="/home">Home</Link>
             </Menu.Item>
             <Menu.Item key="2">
-                <Link to="/scene_illustration">Scene Illustration</Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-                <Link to="/script_editing">Script Editing</Link>
-            </Menu.Item>
-            <Menu.Item key="4">
                 <Link to="/context">Context Management</Link>
             </Menu.Item>
         </Menu>
