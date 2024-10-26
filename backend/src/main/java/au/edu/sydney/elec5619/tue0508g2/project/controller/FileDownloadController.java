22264dd1bc24c48eaf693a8f88a4625042b92211
Bill Li <huli0518@uni.sydney.edu.au>
2024-10-27 00:53:38 +1100

    fix: fix bugs when merging from develop branch.

--- backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/controller/FileDownloadController.java
+++ backend/src/main/java/au/edu/sydney/elec5619/tue0508g2/project/controller/FileDownloadController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
